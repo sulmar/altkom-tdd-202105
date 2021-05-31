508f3c2333d1ebd2dc43da35e3e43c431b408c77
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-05-31 14:37:29 +0200

    Kalkulator zamówienia

---
+++ TestApp/Fundamentals/OrderDiscountCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Fundamentals
+{
+    public class Order
+    {
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+
+    // Happy Hours - w godz. 9:00 - 16:00 klient otrzymuje 10% upustu
+
+    public class OrderDiscountCalculator
+    {
+        public decimal CalculateDiscount(Order order)
+        {
+            throw new NotImplementedException();       
+        }
+    }
+}
