a107d9409b2862b24502b33654c88541c0a49491
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-05-31 11:29:59 +0200

    Testowanie metody void

---
+++ TestApp.MSUnitTests/LoggerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.MSUnitTests
+{
+    [TestClass]
+    public class LoggerTests
+    {
+        private Logger logger;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            logger = new Logger();
+        }
+
+        [TestMethod]
+        public void Log_ValidMessage_SetLastMessage()
+        {
+            // Act
+            logger.Log("a");
+
+            // Assert
+            Assert.AreEqual(expected: logger.LastMessage, "a");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Log_EmptyMessage_ThrowArgumentNullException()
+        {
+            // Act
+            logger.Log(string.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Log_NullMessage_ThrowArgumentNullException()
+        {
+            // Act
+            logger.Log(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Log_WhiteSpace_ThrowArgumentNullException()
+        {
+            // Act
+            logger.Log(" ");
+        }
+    }
+}
