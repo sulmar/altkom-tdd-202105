c02fbbe6b4aa945e82f00eac26ffa419d9457fda
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-06-01 15:31:22 +0200

    Fake

---
+++ WebApi/FakeRepositories/FakeVehicleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.IRepositories;
+using WebApi.Models;
+
+namespace WebApi.FakeRepositories
+{
+    public class FakeVehicleRepository : IVehicleRepository
+    {
+        public Vehicle Get(int id)
+        {
+            return new Vehicle { Id = 1, Model = "Mazda", Name = "6" };
+        }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.FakeRepositories;
+using WebApi.IRepositories;
 
 namespace WebApi
 {
@@ -25,7 +27,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IVehicleRepository, FakeVehicleRepository>();
             services.AddControllers();
         }
 
